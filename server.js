444f2c316590f046664727fff98b740043e1fafd
Aakashth2025 <aakash1505at@gmail.com>
2026-02-17 01:19:21 +0530

    deployemnt

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const MONGO_URI = process.env.MONGO_URI;
 
@@ -183,6 +183,9 @@ app.get('/test', (req, res) => {
     res.json({ message: 'Test successful' });
 });
 
-app.listen(PORT, () => {
+/*app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
+});*/
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
